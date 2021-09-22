9b3486a1a0c3756bc32d5fcad50be470524ba9bc
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-09-22 09:43:01 -0600

    fixed the information going in

--- server.js
+++ server.js
@@ -1,60 +1,63 @@
 'use strict'
-require('dotenv').config();
-const express = require('express');
-const weatherData = require('./data/weather.json');
-
-const PORT = process.env.PORT || 9002;
+const express = require("express");
 const app = express();
-
+require("dotenv").config();
+const PORT = process.env.PORT || 9001;
+const weatherData = require("./data/weather.json");
+const { response } = require("express");
 
 app.get('/', (request, response) => {
-    response.status(200).send('turn back!')
+    response.status(200).send('It\'s over 9000!')
 });
 
-app.get('/weatherData', (request, response) => {
-try{
-let city = request.query.searchquery;
-const lat =request.query.lat;
-const lon = request.query.lon;
 
-if(city){
-    city=city.toLowerCase();
-};
-
-
-
-const weatherQuery = weatherData.find( searchedCity => searchedCity.city_Name.toLowerCase() === city && searchedCity.lat === lat && searchedCity.lon === lon);
-
-
-if(weatherQuery === true){
-    createWeather(city)
-};
-
-
-
-createWeather(city);
-}
-catch (error){
-response.send(404, `Error Not A City`)
-};
-
-
-response.send(weatherQuery)
-});
 
 class Forecast{
     constructor (date, description){
     this.date = date;
     this.description = description;
     };
-
 }
-function createWeather(city){
-    const weatherReadings = city.weatherData.map((day) => {
+
+
+app.get('/weather', (request, response) => {
+    let city = request.query.searchquery;
+    const lat =request.query.lat;
+    const lon = request.query.lon;
+    if(city){
+        city=city.toLowerCase();
+    };
+
+
+    const cityData = weatherData.find( 
+        (searchedCity) => 
+            searchedCity.city_name.toLowerCase() === city && 
+            searchedCity.lat === lat && 
+            searchedCity.lon === lon
+    );
+
+
+// if(weatherQuery === true){
+//     createWeather(city)
+// };
+
+// try{
+
+// }; catch (error){
+// response.send(404, `Error Not A City`)
+// };
+response.send(createWeather(cityData))
+});
+
+function createWeather(cityData){
+    const weatherReadings = cityData.data.map((day) => {
         const date = day.datetime;
         const description = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
         return new Forecast(date, description)
     });
     return weatherReadings;
-}
+};
+
+
+
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
