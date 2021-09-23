a0ab0358c136f7f4ae0b54f701f987d1eb49f217
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-09-23 16:23:21 -0600

    added read me

---
+++ components/weather.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const { response } = require("express");
+
+function handleGetAbout(req, res){
+    response.send('Hi from about');
+}
+
+function handleGetHome(request)
+
+class Forecast{
+    constructor (date, description){
+    this.date = date;
+    this.description = description;
+    };
+}
