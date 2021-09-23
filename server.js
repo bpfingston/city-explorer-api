d78bca7d0bd14b6a192d8756ed09b13a58a2f317
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-09-23 00:28:45 -0600

    finished it to completion for this lab

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT || 9001;
+const PORT = process.env.PORT || 9002;
 const weatherData = require("./data/weather.json");
+const cors = require('cors');
 const { response } = require("express");
 
 app.get('/', (request, response) => {
     response.status(200).send('It\'s over 9000!')
 });
-
-
+app.use(cors());
 
 class Forecast{
     constructor (date, description){
@@ -19,45 +19,35 @@ class Forecast{
     };
 }
 
-
 app.get('/weather', (request, response) => {
     let city = request.query.searchquery;
-    const lat =request.query.lat;
+    const lat = request.query.lat;
     const lon = request.query.lon;
     if(city){
         city=city.toLowerCase();
     };
 
-
+    try{
     const cityData = weatherData.find( 
         (searchedCity) => 
             searchedCity.city_name.toLowerCase() === city && 
             searchedCity.lat === lat && 
             searchedCity.lon === lon
     );
+    console.log(response.send(createWeather(cityData)), '<--- my stuff');
+    response.send(createWeather(cityData));
+    }catch(error){
+    response.status(404, `Error Not A City`);
+    };
 
-
-// if(weatherQuery === true){
-//     createWeather(city)
-// };
-
-// try{
-
-// }; catch (error){
-// response.send(404, `Error Not A City`)
-// };
-response.send(createWeather(cityData))
-});
-
-function createWeather(cityData){
-    const weatherReadings = cityData.data.map((day) => {
+    function createWeather(cityData){
+        const weatherReadings = cityData.data.map((day) => {
         const date = day.datetime;
         const description = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
         return new Forecast(date, description)
-    });
+        });
     return weatherReadings;
-};
-
-
+    };
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
