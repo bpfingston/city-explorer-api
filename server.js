d9e15a23cee63a087943cf758cb8abc52292949c
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-09-24 00:09:28 -0600

    added component for weather and movie, and finished movie api key

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 9002;
-const weatherData = require("./data/weather.json");
+const axios = require("axios");
 const cors = require("cors");
-// const { response } = require("express");
+const weather = require ("./weather.js");
+const movie = require ("./movie.js");
+
 app.use(cors());
 
 
@@ -13,43 +15,9 @@ app.get('/', (request, response) => {
     response.status(200).send('It\'s over 9000!')
 });
 
+app.get('/weather', weather.grabWeather);
 
-class Forecast{
-    constructor (date, description){
-    this.date = date;
-    this.description = description;
-    };
-}
+app.get('/movie', movie.grabMovie);
 
-app.get('/weather', (request, response) => {
-    let city = request.query.searchquery;
-    const lat = request.query.lat;
-    const lon = request.query.lon;
-
-
-    try{
-        city=city.toLowerCase();
-        
-    const cityData = weatherData.find( 
-        (searchedCity) => 
-            searchedCity.city_name.toLowerCase() === city &&
-            searchedCity.lat === lat && 
-            searchedCity.lon === lon
-    );
-    console.log(createWeather(cityData), '<--- my stuff');
-    response.send(createWeather(cityData));
-    }catch(error){
-    response.status(404, `Error Not A City`);
-    };
-
-    function createWeather(cityData){
-        const weatherReadings = cityData.data.map((day) => {
-        const date = day.datetime;
-        const description = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
-        return new Forecast(date, description)
-        });
-    return weatherReadings;
-    };
-});
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
